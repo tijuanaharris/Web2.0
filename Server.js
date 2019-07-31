bd2f1aa1e6a8976d9ff04523b425c4a8ca2b0f78
tijuanaharris <tijuanamharris@gmail.com>
2019-07-31 18:29:19 -0500

    Posting With Postman

--- Server.js
+++ Server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express ();
+const bodyParser = require('body-parser');
+
+/* handling all the parsing */
+app.use(bodyParser.json());
+
 
 app.post('/', (req,res)=>{
 var email = req.body.email;
